93c36c68caebdf952feb54b3cc1856e9e3dcd4f6
Tomas Di Vito <Tomas Di Vito>
2017-01-04 22:00:02 -0300

    more experiments

--- app/views/workshops/workshops.js
+++ app/views/workshops/workshops.js
@@ -1,5 +1,19 @@
+var gestures = require("ui/gestures");
+
+var page;
+
 var hide_unhide = function(args) {
-  id = args.object.parent.id;
-  alert(id);
+  var toHide = findViewById(page, "workshop_description_1");
+  if(toHide.opacity === 0) {
+    toHide.opacity = 100;
+  }
+  else {
+    toHide.opacity = 0;
+  }
 }
 exports.hide_unhide = hide_unhide;
+
+var pageLoaded = function(args) {
+  page = args.object;
+}
+exports.pageLoaded = pageLoaded;
