db44adf977ccd45b90464cb57bdc61e163d7f77c
Tomas Di Vito <tomasdv2@gmail.com>
2017-01-18 21:24:14 -0300

    working on some more stuff of notifications

--- app/app.js
+++ app/app.js
@@ -1,6 +1,9 @@
 var application = require("application");
 var firebase = require("nativescript-plugin-firebase");
 var Toast = require("nativescript-toast");
+var dialogs = require("ui/dialogs");
+/*
+	*/
 
 application.cssFile = "./app.css";
 
@@ -8,8 +11,8 @@ firebase.init({
   onMessageReceivedCallback: function(message) {
       console.log("Title: " + message.title);
       console.log("Body: " + message.body);
-      //alert(message.title + ": " + message.body);
-      var toast = Toast.makeText(message.title + ": " + message.body, "long");
+
+      var toast = Toast.makeText(message.body, "long");
       toast.show();
 
       // if your server passed a custom property called 'foo', then do this:
@@ -23,5 +26,9 @@ firebase.init({
       console.log("firebase.init error: " + error);
     }
 );
+
+//uncomment next line when implemmented in the plugin :(
+firebase.subscribeToTopic("flasoma");
+console.log("subscribed to flasoma")
 
 application.start({ moduleName: "main-page" });
