f682fdd5bb63520ee2db97af9ce437c8e82b6d92
Tomas Di Vito <tomasdv2@gmail.com>
2017-01-20 12:35:49 -0300

    Added new icons for android

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ var application = require("application");
 var firebase = require("nativescript-plugin-firebase");
 var Toast = require("nativescript-toast");
 var dialogs = require("ui/dialogs");
+var SweetAlert = require('ns-sweet-alert');
 /*
 	*/
 
@@ -11,9 +12,14 @@ firebase.init({
   onMessageReceivedCallback: function(message) {
       console.log("Title: " + message.title);
       console.log("Body: " + message.body);
-
-      var toast = Toast.makeText(message.body, "long");
-      toast.show();
+      if(application.android) {
+        SweetAlert.simpleAlert(message.body, application.android.foregroundActivity);
+      }
+      else {
+        alert(message.body);
+      }
+      //var toast = Toast.makeText(message.body, "long");
+      //toast.show();
     }
 }).then(
     function (instance) {
