35183db19ae24ffe67c5bf4505f2591868a7923e
Tomas Di Vito <tomasdv2@gmail.com>
2017-01-24 00:10:09 -0300

    fixed bug in the naming of the images

--- app/views/ComiteColaboradores/comitecolaboradores.js
+++ app/views/ComiteColaboradores/comitecolaboradores.js
@@ -5,14 +5,14 @@ var layout = require("ui/layouts/grid-layout");
 
 var colaboradores = [
   {name: "Adrian Garde", subtitle: "RRPP", imgPath: "~/images/comite/colaboradores/adrian garde - rrpp.png"},
-  {name: "Alexander", subtitle: "Secretaria Administrativa", imgPath: "~/images/comite/colaboradores/alexander - secretaría administrativa.png"},
+  {name: "Alexander", subtitle: "Secretaria Administrativa", imgPath: "~/images/comite/colaboradores/alexander - secretaria administrativa.png"},
   {name: "Daniel Raley", subtitle: "Feria de dealers", imgPath: "~/images/comite/colaboradores/daniel raley - feria de dealers.png"},
-  {name: "Fantasio", subtitle: "Asesor artistico", imgPath: "~/images/comite/colaboradores/fantasio - asesor artístico.png"},
+  {name: "Fantasio", subtitle: "Asesor artistico", imgPath: "~/images/comite/colaboradores/fantasio - asesor artistico.png"},
   {name: "Gustavo Valentini", subtitle: "Director de Competencias", imgPath: "~/images/comite/colaboradores/gustavo valentini - director de competencias.png"},
-  {name: "Henry Evans", subtitle: "Asesor artistico", imgPath: "~/images/comite/colaboradores/henry evans . asesor artístico.png"},
-  {name: "Multtán", subtitle: "Director artistico", imgPath: "~/images/comite/colaboradores/multtán - director artístico.png"},
+  {name: "Henry Evans", subtitle: "Asesor artistico", imgPath: "~/images/comite/colaboradores/henry evans - asesor artistico.png"},
+  {name: "Multtán", subtitle: "Director artistico", imgPath: "~/images/comite/colaboradores/multtan - director artistico.png"},
   {name: "Natalia Cahiza", subtitle: "RRPP", imgPath: "~/images/comite/colaboradores/natalia cahiza - rrpp.png"},
-  {name: "Ramsor", subtitle: "Responsable de hoteleria", imgPath: "~/images/comite/colaboradores/ramsor - responsable de hotelería.png"},
+  {name: "Ramsor", subtitle: "Responsable de hoteleria", imgPath: "~/images/comite/colaboradores/ramsor - responsable de hoteleria.png"},
 ]
 
 var loadGridGallery = function(gg) {
